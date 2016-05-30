e526fceb42da8a3b1aaf2af2b1a066415f3147ef
Jens Rene Suckert <jens.rene.suckert@rwth-aachen.de>
2016-05-30 12:44:36 +0200

    Commented out Eigenvalue printing

--- reference/src/main.cpp
+++ reference/src/main.cpp
@@ -313,10 +313,12 @@ int main(int argc, char* argv[])
     reset_clock();
     std::cout << "Filtered Vectors\t\t" << filteredVecs << std::endl;
 
+    /*
     std::cout << "Eigenvalues: " << std::endl;
     for(int zzt = 0; zzt < nev; zzt++)
       std::cout << Lambda[zzt] << std::endl;
     std::cout << "End of eigenvalues. " << std::endl;
+    */
   } // for(int i = bgn; i <= end; ++i)
 
 
