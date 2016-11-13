cf8872230f09d1c5d37313215a134198375c3d97
Jan Winkelmann <winkelmann@aices.rwth-aachen.de>
2016-11-13 14:07:02 +0100

    lowerb is now an average of old and new lowerb

--- src/chase.cpp
+++ src/chase.cpp
@@ -54,7 +54,8 @@ void chase(MKL_Complex16* const H, std::size_t N, MKL_Complex16* V, MKL_Complex1
   {
     if(unconverged < nevex || iteration == 0){
       lambda = * std::min_element( ritzv_, ritzv_ + nevex );
-      lowerb = * std::max_element( ritzv, ritzv + unconverged );
+      auto tmp = * std::max_element( ritzv, ritzv + unconverged );
+      lowerb = (lowerb + tmp ) / 2;
       //upperb = lowerb + std::abs(lowerb - lambda);
     }
 #ifdef OUTPUT
@@ -64,7 +65,11 @@ void chase(MKL_Complex16* const H, std::size_t N, MKL_Complex16* V, MKL_Complex1
       << " " << unconverged
       << std::endl;
 #endif
-    assert( lowerb < upperb );
+    //    assert( lowerb < upperb );
+    if( lowerb < upperb ) {
+      std::cout << "ASSERTION FAILURE lowerb < upperb\n";
+      lowerb = upperb;
+    }
     //--------------------------------- FILTER ---------------------------------
     if( int_opt != CHASE_OPT_NONE && iteration != 0 )
     {
