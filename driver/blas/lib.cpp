8d385701b7362b0c47ebe7256ff202ed09876214
Jan Winkelmann <winkelmann@aices.rwth-aachen.de>
2018-01-22 16:51:25 +0100

    new Blas lib
    fixes for non-complex chase

--- driver/blas/lib.cpp
+++ driver/blas/lib.cpp
@@ -1,13 +1,13 @@
 /* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
 #include <complex.h>
-#include <hdf5.h>
-#include <mpi.h>
+//#include <hdf5.h>
+//#include <mpi.h>
+#include <functional>
 #include <random>
 
 #include "genera/matrixfree/blas_templates.h"
 #include "genera/matrixfree/factory.h"
-
 
 using namespace chase;
 
@@ -53,69 +53,120 @@ ChASE_Blas<std::complex<float> >(config, H, V, ritzv);
     single->solve();
     std::cout << ritzv[0] << "\n";
 
-    delete single;
 }
 */
+/*
 void c_chase_(MPI_Fint* Fcomm, std::complex<float>* H, int* N,
-              std::complex<float>* V, float* ritzv, int* nev, int* nex,
-              int* deg, double* tol, char* mode, char* opt) {
-  MPI_Comm comm = MPI_Comm_f2c(*Fcomm);
-  int mpi_size = 0;
-  MPI_Comm_size(comm, &mpi_size);
+             std::complex<float>* V, float* ritzv, int* nev, int* nex,
+             int* deg, double* tol, char* mode, char* opt) {
+ MPI_Comm comm = MPI_Comm_f2c(*Fcomm);
+ int mpi_size = 0;
+ MPI_Comm_size(comm, &mpi_size);
+
+ std::mt19937 gen(2342.0);  // TODO
+ std::normal_distribution<> d;
+ size_t dims_ret[2];
+
+ if (mpi_size > 1) {
+   chase_write_hdf5(comm, H, *N);
+ }
+ MPI_Barrier(comm);
+
+ // if (mpi_size > 1) {
+ //   chase_write_hdf5(comm, H, *N);
+ //   chase_read_matrix(comm, dims_ret, &HH);
+ //   assert(dims_ret[0] == dims_ret[1]);
+ // } else {
+ //   dims_ret[0] = *N;
+ //   dims_ret[1] = *N;
+ //   HH = H;
+ // }
+
+ ChaseConfig<std::complex<float>> config(*N, *nev, *nex);
+ config.setTol(*tol);
+ config.setDeg(*deg);
+ config.setOpt(*opt == 'S' || *opt == 's');
+ config.setApprox(*mode == 'A' || *mode == 'a');
+ config.setLanczosIter(25);
+
+ std::unique_ptr<MatrixFreeChase<T>> single =
+   constructChASE(config, static_cast<T*>(nullptr), V, ritzv, MPI_COMM_WORLD);
+
+ CHASE_INT xoff;
+ CHASE_INT yoff;
+ CHASE_INT xlen;
+ CHASE_INT ylen;
+
+ single->GetOff(&xoff, &yoff, &xlen, &ylen);
+
+ std::complex<float>* HH = single->GetMatrixPtr();
+ if (mpi_size == 1) {
+   for (auto i = 0; i < *N * *N; ++i) HH[i] = H[i];
+
+ } else {
+   chase_read_matrix(comm, xoff, yoff, xlen, ylen, HH);
+ }
+
+ // single = new ChASE_Blas<std::complex<float> >(config, HH, V, ritzv);
+
+ if (!config.use_approx())
+   for (std::size_t k = 0; k < *N * (*nev + *nex); ++k)
+     V[k] = std::complex<float>(d(gen), d(gen));
+
+ //    float normH = std::max<float>(t_lange('1', *N, *N, HH, *N), float(1.0));
+ single->SetNorm(6);
+
+ single->Solve();
+}
+*/
+
+void c_zchase_(std::complex<double>* H, int* N, std::complex<double>* V,
+               double* ritzv, int* nev, int* nex, int* deg, double* tol,
+               char* mode, char* opt) {
+  std::cout << "entering chase" << std::endl;
+  std::cout << "tol: " << *tol << std::endl;
+  ChaseConfig<std::complex<double>> config(*N, *nev, *nex);
+
+  config.setTol(*tol);
+  config.setDeg(*deg);
+  config.setOpt(*opt == 'S');
+  config.setApprox(*mode == 'A');
 
   std::mt19937 gen(2342.0);  // TODO
   std::normal_distribution<> d;
-  size_t dims_ret[2];
-
-  if (mpi_size > 1) {
-    chase_write_hdf5(comm, H, *N);
-  }
-  MPI_Barrier(comm);
-
-  // if (mpi_size > 1) {
-  //   chase_write_hdf5(comm, H, *N);
-  //   chase_read_matrix(comm, dims_ret, &HH);
-  //   assert(dims_ret[0] == dims_ret[1]);
-  // } else {
-  //   dims_ret[0] = *N;
-  //   dims_ret[1] = *N;
-  //   HH = H;
-  // }
-
-  ChaseConfig<std::complex<float>> config(*N, *nev, *nex);
-  config.setTol(*tol);
-  config.setDeg(*deg);
-  config.setOpt(*opt == 'S' || *opt == 's');
-  config.setApprox(*mode == 'A' || *mode == 'a');
-  config.setLanczosIter(25);
-
-  std::unique_ptr<MatrixFreeChase<T>> single =
-    constructChASE(config, static_cast<T*>(nullptr), V, ritzv, MPI_COMM_WORLD);
-
-  CHASE_INT xoff;
-  CHASE_INT yoff;
-  CHASE_INT xlen;
-  CHASE_INT ylen;
-
-  single->GetOff(&xoff, &yoff, &xlen, &ylen);
-
-  std::complex<float>* HH = single->GetMatrixPtr();
-  if (mpi_size == 1) {
-    for (auto i = 0; i < *N * *N; ++i) HH[i] = H[i];
-
-  } else {
-    chase_read_matrix(comm, xoff, yoff, xlen, ylen, HH);
-  }
-
-  // single = new ChASE_Blas<std::complex<float> >(config, HH, V, ritzv);
 
   if (!config.use_approx())
     for (std::size_t k = 0; k < *N * (*nev + *nex); ++k)
-      V[k] = std::complex<float>(d(gen), d(gen));
+      V[k] = std::complex<double>(d(gen), d(gen));
 
-  //    float normH = std::max<float>(t_lange('1', *N, *N, HH, *N), float(1.0));
-  single->SetNorm(6);
+  std::unique_ptr<MatrixFreeChase<std::complex<double>>> single_ =
+      constructChASE(config, H, V, ritzv, MPI_COMM_SELF);
 
-  single->Solve();
+  single_->Solve();
+
+}
+
+void c_dchase_(double* H, int* N, double* V, double* ritzv, int* nev, int* nex,
+               int* deg, double* tol, char* mode, char* opt) {
+  std::cout << "entering chase" << std::endl;
+  std::cout << "tol: " << *tol << std::endl;
+  ChaseConfig<double> config(*N, *nev, *nex);
+
+  config.setTol(*tol);
+  config.setDeg(*deg);
+  config.setOpt(*opt == 'S');
+  config.setApprox(*mode == 'A');
+
+  std::mt19937 gen(2342.0);  // TODO
+  std::normal_distribution<> d;
+
+  if (!config.use_approx())
+    for (std::size_t k = 0; k < *N * (*nev + *nex); ++k) V[k] = d(gen);
+
+  std::unique_ptr<MatrixFreeChase<double>> single_ =
+      constructChASE(config, H, V, ritzv, MPI_COMM_SELF);
+
+  single_->Solve();
+
 }
 }
