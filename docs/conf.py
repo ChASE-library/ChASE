d5774c08c0de8134c80fe9b6979d9803128b2eca
Xinzhe Wu <xin.wu@fz-juelich.de>
2021-03-01 14:15:51 +0100

    minor changes to include custormized templates of Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,7 @@ todo_include_todos = True
 # a list of builtin themes.
 # html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
-#html_theme_path = ["_themes", ]
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -166,7 +166,7 @@ html_favicon = "images/ChASE_Logo_Favicon_RGB.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['css']
+html_static_path = ['_static']
 
 html_context = {
     'css_files': [
