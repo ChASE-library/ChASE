5f3aaeb2625849c2b095094cfa83aba28cfeceb8
brunowu <xinzhe.wu1990@gmail.com>
2021-07-01 16:44:22 +0200

    update copyright information

--- ChASE-MPI/kernels/shift.cu
+++ ChASE-MPI/kernels/shift.cu
@@ -1,3 +1,10 @@
+/* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+// This file is a part of ChASE.
+// Copyright (c) 2015-2021, Simulation and Data Laboratory Quantum Materials, 
+//   Forschungszentrum Juelich GmbH, Germany. All rights reserved.
+// License is 3-clause BSD:
+// https://github.com/ChASE-library/ChASE
+
 #include <cuComplex.h>
 #include <cuda_runtime.h>
 #include <omp.h>
--- algorithm/algorithm.hpp
+++ algorithm/algorithm.hpp
@@ -1,12 +1,9 @@
 /* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 // This file is a part of ChASE.
-// Copyright (c) 2015-2018, Simulation Laboratory Quantum Materials,
-//   Forschungszentrum Juelich GmbH, Germany
-// and
-// Copyright (c) 2016-2018, Aachen Institute for Advanced Study in Computational
-//   Engineering Science, RWTH Aachen University, Germany All rights reserved.
+// Copyright (c) 2015-2021, Simulation and Data Laboratory Quantum Materials, 
+//   Forschungszentrum Juelich GmbH, Germany. All rights reserved.
 // License is 3-clause BSD:
-// https://github.com/SimLabQuantumMaterials/ChASE/
+// https://github.com/ChASE-library/ChASE
 
 #ifndef CHASE_ALGORITHM_ALGORITHM_HPP
 #define CHASE_ALGORITHM_ALGORITHM_HPP
