d45bf40dcd47f9a6265a74a63376a903668de19d
Xinzhe Wu <xin.wu@fz-juelich.de>
2022-06-07 20:48:16 +0200

    add interface with single precsion scalar

--- interface/chase_c.cpp
+++ interface/chase_c.cpp
@@ -11,6 +11,9 @@
 #include <complex.h>
 #include <mpi.h>
 #include <random>
+#include <chrono>
+
+#include "algorithm/performance.hpp"
 
 #include "ChASE-MPI/chase_mpi.hpp"
 #include "ChASE-MPI/chase_mpi_properties.hpp"
@@ -18,10 +21,9 @@
 #include "ChASE-MPI/impl/chase_mpidla_blaslapack_seq.hpp"
 #include "ChASE-MPI/impl/chase_mpidla_blaslapack_seq_inplace.hpp"
 
-#ifdef CHASE_USE_GPU
+#ifdef HAS_GPU
   #include "ChASE-MPI/impl/chase_mpidla_mgpu.hpp"
 #endif
-
 
 using namespace chase;
 using namespace chase::mpi;
@@ -57,10 +59,14 @@ class ChASE_State {
 
   static ChaseMpiProperties<double>* double_prec;
   static ChaseMpiProperties<std::complex<double>>* complex_double_prec;
+  static ChaseMpiProperties<float>* single_prec;
+  static ChaseMpiProperties<std::complex<float>>* complex_single_prec;  
 };
 
 ChaseMpiProperties<double>* ChASE_State::double_prec = nullptr;
 ChaseMpiProperties<std::complex<double>>* ChASE_State::complex_double_prec = nullptr;
+ChaseMpiProperties<float>* ChASE_State::single_prec = nullptr;
+ChaseMpiProperties<std::complex<float>>* ChASE_State::complex_single_prec = nullptr;
 
 template <>
 ChaseMpiProperties<double>* ChASE_State::constructProperties(std::size_t N,
@@ -97,6 +103,40 @@ ChaseMpiProperties<std::complex<double>>* ChASE_State::constructProperties(std::
 }
 
 
+template <>
+ChaseMpiProperties<float>* ChASE_State::constructProperties(std::size_t N,
+                                                             std::size_t mbsize,
+                                                             std::size_t nbsize,
+                                                             std::size_t nev,
+                                                             std::size_t nex,
+                                                             int dim0,
+                                                             int dim1,
+                                                             char *grid_major,
+                                                             int irsrc,
+                                                             int icsrc,
+                                                             MPI_Comm comm) {
+  single_prec = new ChaseMpiProperties<float>(N, mbsize, nbsize, nev, nex, dim0,
+                                                dim1, grid_major, irsrc, icsrc,comm);
+  return single_prec;
+}
+
+template <>
+ChaseMpiProperties<std::complex<float>>* ChASE_State::constructProperties(std::size_t N,
+                                                             std::size_t mbsize,
+                                                             std::size_t nbsize,
+                                                             std::size_t nev,
+                                                             std::size_t nex,
+                                                             int dim0,
+                                                             int dim1,
+                                                             char *grid_major,
+                                                             int irsrc,
+                                                             int icsrc,
+                                                             MPI_Comm comm) {
+  complex_single_prec = new ChaseMpiProperties<std::complex<float>>(N, mbsize, nbsize, nev, nex, dim0,
+                                                dim1, grid_major, irsrc, icsrc,comm);
+  return complex_single_prec;
+}
+
 
 template <>
 ChaseMpiProperties<double>* ChASE_State::getProperties() {
@@ -107,11 +147,24 @@ ChaseMpiProperties<std::complex<double>>* ChASE_State::getProperties() {
   return complex_double_prec;
 }
 
+template <>
+ChaseMpiProperties<float>* ChASE_State::getProperties() {
+  return single_prec;
+}
+template <>
+ChaseMpiProperties<std::complex<float>>* ChASE_State::getProperties() {
+  return complex_single_prec;
+}
+
 template <typename T>
-void call_chase(T* H, int* N, T* V, Base<T>* ritzv, int* nev, int* nex,
+void chase_seq(T* H, int* N, T* V, Base<T>* ritzv, int* nev, int* nex,
                 int* deg, double* tol, char* mode, char* opt) {
   typedef ChaseMpi<ChaseMpiDLABlaslapackSeq, T> SEQ_CHASE;
-  std::cerr << "entering chase" << std::endl;
+
+  std::vector<std::chrono::duration<double>> timings(3);
+  std::vector<std::chrono::time_point<std::chrono::high_resolution_clock>> start_times(3);
+  
+  start_times[1] = std::chrono::high_resolution_clock::now();
 
   std::mt19937 gen(2342.0);
   std::normal_distribution<> d;
@@ -128,7 +181,16 @@ void call_chase(T* H, int* N, T* V, Base<T>* ritzv, int* nev, int* nex,
     for (std::size_t k = 0; k < *N * (*nev + *nex); ++k)
       V[k] = getRandomT<T>([&]() { return d(gen); });
 
-  chase::Solve(&single);
+  PerformanceDecoratorChase<T> performanceDecorator(&single);
+  start_times[2] = std::chrono::high_resolution_clock::now();
+  chase::Solve(&performanceDecorator);
+  timings[2] = std::chrono::high_resolution_clock::now() - start_times[2];
+  std::cout << "ChASE]> Seq-ChASE Solve done in: " << timings[2].count() << "\n";
+
+  performanceDecorator.GetPerfData().print();
+  
+  timings[1] = std::chrono::high_resolution_clock::now() - start_times[1];
+  std::cout << "ChASE]> total time in ChASE: " << timings[1].count() << "\n";
 }
 
 template <typename T>
@@ -142,16 +204,13 @@ void chase_setup(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, in
 template <typename T>
 void chase_solve(T* H, T* V, Base<T>* ritzv, int* deg, double* tol, char* mode,
                  char* opt) {
-#ifdef CHASE_USE_GPU
-  typedef ChaseMpi<ChaseMpiDLAMultiGPU, T> CHASE;	
-#else
   typedef ChaseMpi<ChaseMpiDLABlaslapack, T> CHASE;
-#endif
-  std::cerr << "entering chase" << std::endl;
 
   std::mt19937 gen(2342.0);
   std::normal_distribution<> d;
   ChaseMpiProperties<T>* props = ChASE_State::getProperties<T>();
+
+  int myRank = props->get_my_rank();
 
   CHASE single(props, V, ritzv);
 
@@ -178,22 +237,73 @@ void chase_solve(T* H, T* V, Base<T>* ritzv, int* deg, double* tol, char* mode,
 
   chase::Solve(&single);
 }
+#ifdef HAS_GPU
+template <typename T>
+void chase_solve_mgpu(T* H, T* V, Base<T>* ritzv, int* deg, double* tol, char* mode,
+                 char* opt) {
+  
+  typedef ChaseMpi<ChaseMpiDLAMultiGPU, T> CHASE;	
+
+  std::mt19937 gen(2342.0);
+  std::normal_distribution<> d;
+  ChaseMpiProperties<T>* props = ChASE_State::getProperties<T>();
+
+  int myRank = props->get_my_rank();
+
+  CHASE single(props, V, ritzv);
+
+  T* H_ = single.GetMatrixPtr();
+  std::size_t m, n;
+  m = props->get_m();
+  n = props->get_n();
+
+  ChaseConfig<T>& config = single.GetConfig();
+  auto N = config.GetN();
+  auto nev = config.GetNev();
+  auto nex = config.GetNex();
+
+  if (!config.UseApprox())
+    for (std::size_t k = 0; k < N * (nev + nex); ++k)
+      V[k] = getRandomT<T>([&]() { return d(gen); });
+
+  for (std::size_t k = 0; k < m * n; ++k) H_[k] = H[k];
+
+  config.SetTol(*tol);
+  config.SetDeg(*deg);
+  config.SetOpt(*opt == 'S');
+  config.SetApprox(*mode == 'A');
+
+  chase::Solve(&single);
+}
+#endif
 
 extern "C" {
 
 void zchase_(std::complex<double>* H, int* N, std::complex<double>* V,
              double* ritzv, int* nev, int* nex, int* deg, double* tol,
              char* mode, char* opt) {
-  call_chase<std::complex<double>>(H, N, V, ritzv, nev, nex, deg, tol, mode,
+  chase_seq<std::complex<double>>(H, N, V, ritzv, nev, nex, deg, tol, mode,
                                    opt);
 }
 
 void dchase_(double* H, int* N, double* V, double* ritzv, int* nev, int* nex,
              int* deg, double* tol, char* mode, char* opt) {
-  call_chase<double>(H, N, V, ritzv, nev, nex, deg, tol, mode, opt);
+  chase_seq<double>(H, N, V, ritzv, nev, nex, deg, tol, mode, opt);
 }
 
-void zchase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, int* nex, 
+void cchase_(std::complex<float>* H, int* N, std::complex<float>* V,
+             float* ritzv, int* nev, int* nex, int* deg, double* tol,
+             char* mode, char* opt) {
+  chase_seq<std::complex<float>>(H, N, V, ritzv, nev, nex, deg, tol, mode,
+                                   opt);
+}
+
+void schase_(float* H, int* N, float* V, float* ritzv, int* nev, int* nex,
+             int* deg, double* tol, char* mode, char* opt) {
+  chase_seq<float>(H, N, V, ritzv, nev, nex, deg, tol, mode, opt);
+}
+
+void pzchase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, int* nex, 
 		int *dim0, int *dim1, char *grid_major, int *irsrc, int *icsrc){
 
   chase_setup<std::complex<double>>(fcomm, N, mbsize, nbsize, nev, nex, dim0, dim1,
@@ -201,7 +311,7 @@ void zchase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, in
 
 }
 
-void dchase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, int* nex,
+void pdchase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, int* nex,
                 int *dim0, int *dim1, char *grid_major, int *irsrc, int *icsrc){
 
   chase_setup<double>(fcomm, N, mbsize, nbsize, nev, nex, dim0, dim1,
@@ -209,13 +319,62 @@ void dchase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, in
 
 }
 
-void zchase_solve(std::complex<double>* H, std::complex<double>* V,
+void pcchase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, int* nex,
+                int *dim0, int *dim1, char *grid_major, int *irsrc, int *icsrc){
+
+  chase_setup<std::complex<float>>(fcomm, N, mbsize, nbsize, nev, nex, dim0, dim1,
+                                        grid_major, irsrc, icsrc);
+
+}
+
+void pschase_init(MPI_Fint* fcomm, int* N, int *mbsize, int *nbsize, int* nev, int* nex,
+                int *dim0, int *dim1, char *grid_major, int *irsrc, int *icsrc){
+
+  chase_setup<float>(fcomm, N, mbsize, nbsize, nev, nex, dim0, dim1,
+                                        grid_major, irsrc, icsrc);
+
+}
+
+void pzchase_(std::complex<double>* H, std::complex<double>* V,
                   double* ritzv, int* deg, double* tol, char* mode, char* opt) {
   chase_solve<std::complex<double>>(H, V, ritzv, deg, tol, mode, opt);
 }
 
-void dchase_solve(double* H, double* V, double* ritzv, int* deg, double* tol,
+void pdchase_(double* H, double* V, double* ritzv, int* deg, double* tol,
                   char* mode, char* opt) {
   chase_solve<double>(H, V, ritzv, deg, tol, mode, opt);
 }
+
+void pcchase_(std::complex<float>* H, std::complex<float>* V,
+                  float* ritzv, int* deg, double* tol, char* mode, char* opt) {
+  chase_solve<std::complex<float>>(H, V, ritzv, deg, tol, mode, opt);
+}
+
+void pschase_(float* H, float* V, float* ritzv, int* deg, double* tol,
+                  char* mode, char* opt) {
+  chase_solve<float>(H, V, ritzv, deg, tol, mode, opt);
+}
+
+#ifdef HAS_GPU
+void pzchase_mgpu_(std::complex<double>* H, std::complex<double>* V,
+                  double* ritzv, int* deg, double* tol, char* mode, char* opt) {
+  chase_solve_mgpu<std::complex<double>>(H, V, ritzv, deg, tol, mode, opt);
+}
+
+void pdchase_mgpu_(double* H, double* V, double* ritzv, int* deg, double* tol,
+                  char* mode, char* opt) {
+  chase_solve_mgpu<double>(H, V, ritzv, deg, tol, mode, opt);
+}
+
+void pcchase_mgpu_(std::complex<float>* H, std::complex<float>* V,
+                  float* ritzv, int* deg, double* tol, char* mode, char* opt) {
+  chase_solve_mgpu<std::complex<float>>(H, V, ritzv, deg, tol, mode, opt);
+}
+
+void pschase_mgpu_(float* H, float* V, float* ritzv, int* deg, double* tol,
+                  char* mode, char* opt) {
+  chase_solve_mgpu<float>(H, V, ritzv, deg, tol, mode, opt);
+}
+#endif
+
 }  // extern C 
--- interface/chase_f.f90
+++ interface/chase_f.f90
@@ -1,38 +1,76 @@
 MODULE chase_diag
   ! non-MPI
   INTERFACE
-    SUBROUTINE rchase( h, n, v, ritzv, nev, nex, deg, tol, mode, opt ) bind( c, name = 'dchase_' )
+    SUBROUTINE dchase( h, n, v, ritzv, nev, nex, deg, tol, mode, opt ) bind( c, name = 'dchase_' )
       USE, INTRINSIC :: iso_c_binding
       REAL(c_double)        :: h(n,*), v(n,*)
       INTEGER(c_int)                :: n, deg, nev, nex
       REAL(c_double)                :: ritzv(*), tol
       CHARACTER(len=1,kind=c_char)  :: mode, opt
-    END SUBROUTINE rchase
+    END SUBROUTINE dchase
   END INTERFACE
 
   INTERFACE
-    SUBROUTINE cchase( h, n, v, ritzv, nev, nex, deg, tol, mode, opt ) bind( c, name = 'zchase_' )
+    SUBROUTINE schase( h, n, v, ritzv, nev, nex, deg, tol, mode, opt ) bind( c, name = 'schase_' )
       USE, INTRINSIC :: iso_c_binding
-      COMPLEX(c_double_complex)     :: h(n,*), v(n,*)
+      REAL(c_float)        :: h(n,*), v(n,*)
+      INTEGER(c_int)                :: n, deg, nev, nex
+      REAL(c_double)                :: ritzv(*), tol
+      CHARACTER(len=1,kind=c_char)  :: mode, opt
+    END SUBROUTINE schase
+  END INTERFACE
+
+  INTERFACE
+    SUBROUTINE cchase( h, n, v, ritzv, nev, nex, deg, tol, mode, opt ) bind( c, name = 'cchase_' )
+      USE, INTRINSIC :: iso_c_binding
+      COMPLEX(c_float_complex)     :: h(n,*), v(n,*)
       INTEGER(c_int)                :: n, deg, nev, nex
       REAL(c_double)                :: ritzv(*), tol
       CHARACTER(len=1,kind=c_char)  :: mode, opt
     END SUBROUTINE cchase
   END INTERFACE
 
+  INTERFACE
+    SUBROUTINE zchase( h, n, v, ritzv, nev, nex, deg, tol, mode, opt ) bind( c, name = 'zchase_' )
+      USE, INTRINSIC :: iso_c_binding
+      COMPLEX(c_double_complex)     :: h(n,*), v(n,*)
+      INTEGER(c_int)                :: n, deg, nev, nex
+      REAL(c_double)                :: ritzv(*), tol
+      CHARACTER(len=1,kind=c_char)  :: mode, opt
+    END SUBROUTINE zchase
+  END INTERFACE
+
   ! MPI
   INTERFACE
-     SUBROUTINE prchase_init( mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, grid_major, irsrc, icsrc) & 
-                     BIND( c, name = 'dchase_init' )
+     SUBROUTINE pdchase_init( mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, grid_major, irsrc, icsrc) & 
+                     BIND( c, name = 'pdchase_init' )
        USE, INTRINSIC                :: iso_c_binding
        INTEGER(c_int)                :: mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, irsrc, icsrc
        CHARACTER(len=1,kind=c_char)  :: grid_major
-     END SUBROUTINE prchase_init
+     END SUBROUTINE pdchase_init
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE pzchase_init( mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, grid_major, irsrc, icsrc) &
+                     BIND( c, name = 'pzchase_init' )
+       USE, INTRINSIC                :: iso_c_binding
+       INTEGER(c_int)                :: mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, irsrc, icsrc
+       CHARACTER(len=1,kind=c_char)  :: grid_major
+     END SUBROUTINE pzchase_init
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE pschase_init( mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, grid_major, irsrc, icsrc) &
+                     BIND( c, name = 'pschase_init' )
+       USE, INTRINSIC                :: iso_c_binding
+       INTEGER(c_int)                :: mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, irsrc, icsrc
+       CHARACTER(len=1,kind=c_char)  :: grid_major
+     END SUBROUTINE pschase_init
   END INTERFACE
 
   INTERFACE
      SUBROUTINE pcchase_init( mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, grid_major, irsrc, icsrc) &
-                     BIND( c, name = 'zchase_init' )
+                     BIND( c, name = 'pcchase_init' )
        USE, INTRINSIC                :: iso_c_binding
        INTEGER(c_int)                :: mpi_comm, n, mbsize, nbsize, nev, nex, dim0, dim1, irsrc, icsrc
        CHARACTER(len=1,kind=c_char)  :: grid_major
@@ -40,23 +78,89 @@ MODULE chase_diag
   END INTERFACE
 
   INTERFACE
-     SUBROUTINE prchase(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'dchase_solve' )
+     SUBROUTINE pdchase(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pdchase_' )
        USE, INTRINSIC :: iso_c_binding
        REAL(c_double)        :: h(*), v(*)
        INTEGER(c_int)                :: deg
        REAL(c_double)                :: ritzv(*), tol
        CHARACTER(len=1,kind=c_char)  :: mode, opt
-     END SUBROUTINE prchase
+     END SUBROUTINE pdchase
   END INTERFACE
 
   INTERFACE
-     SUBROUTINE pcchase(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'zchase_solve' )
+     SUBROUTINE pzchase(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pzchase_' )
        USE, INTRINSIC :: iso_c_binding
        COMPLEX(c_double_complex)     :: h(*), v(*)
        INTEGER(c_int)                :: deg
        REAL(c_double)                :: ritzv(*), tol
        CHARACTER(len=1,kind=c_char)  :: mode, opt
+     END SUBROUTINE pzchase
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE pschase(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pschase_' )
+       USE, INTRINSIC :: iso_c_binding
+       REAL(c_float)                 :: h(*), v(*)
+       INTEGER(c_int)                :: deg
+       REAL(c_float)                 :: ritzv(*)
+       REAL(c_double)                :: tol
+       CHARACTER(len=1,kind=c_char)  :: mode, opt
+     END SUBROUTINE pschase
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE pcchase(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pcchase_' )
+       USE, INTRINSIC :: iso_c_binding
+       COMPLEX(c_float_complex)      :: h(*), v(*)
+       INTEGER(c_int)                :: deg
+       REAL(c_float)                 :: ritzv(*)
+       REAL(c_double)                :: tol
+       CHARACTER(len=1,kind=c_char)  :: mode, opt
      END SUBROUTINE pcchase
   END INTERFACE
+
+#if defined(HAS_GPU)
+  INTERFACE
+     SUBROUTINE pdchase_mgpu(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pdchase_mgpu_' )
+       USE, INTRINSIC :: iso_c_binding
+       REAL(c_double)        :: h(*), v(*)
+       INTEGER(c_int)                :: deg
+       REAL(c_double)                :: ritzv(*), tol
+       CHARACTER(len=1,kind=c_char)  :: mode, opt
+     END SUBROUTINE pdchase_mgpu
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE pzchase_mgpu(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pzchase_mgpu_' )
+       USE, INTRINSIC :: iso_c_binding
+       COMPLEX(c_double_complex)     :: h(*), v(*)
+       INTEGER(c_int)                :: deg
+       REAL(c_double)                :: ritzv(*), tol
+       CHARACTER(len=1,kind=c_char)  :: mode, opt
+     END SUBROUTINE pzchase_mgpu
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE pschase_mgpu(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pschase_mgpu_' )
+       USE, INTRINSIC :: iso_c_binding
+       REAL(c_float)                 :: h(*), v(*)
+       INTEGER(c_int)                :: deg
+       REAL(c_float)                 :: ritzv(*)
+       REAL(c_double)                :: tol
+       CHARACTER(len=1,kind=c_char)  :: mode, opt
+     END SUBROUTINE pschase_mgpu
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE pcchase_mgpu(h, v, ritzv, deg, tol, mode, opt ) BIND( c, name = 'pcchase_mgpu_' )
+       USE, INTRINSIC :: iso_c_binding
+       COMPLEX(c_float_complex)      :: h(*), v(*)
+       INTEGER(c_int)                :: deg
+       REAL(c_float)                 :: ritzv(*)
+       REAL(c_double)                :: tol
+       CHARACTER(len=1,kind=c_char)  :: mode, opt
+     END SUBROUTINE pcchase_mgpu
+  END INTERFACE  
+#endif
 
 END MODULE chase_diag
