8fef4ab171388793df2cbfa5bcf7ec9951c48ec3
brunowu <xinzhe.wu1990@gmail.com>
2023-03-01 16:57:53 +0100

    update comments in codes

--- ChASE-MPI/chase_mpi_properties.hpp
+++ ChASE-MPI/chase_mpi_properties.hpp
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 // This file is a part of ChASE.
-// Copyright (c) 2015-2021, Simulation and Data Laboratory Quantum Materials,
+// Copyright (c) 2015-2023, Simulation and Data Laboratory Quantum Materials,
 //   Forschungszentrum Juelich GmbH, Germany. All rights reserved.
 // License is 3-clause BSD:
 // https://github.com/ChASE-library/ChASE
