d31a60c490ba51ff90ad0ad9422d2c207ae695c0
Davor Davidović <davor.davidovic@irb.hr>
2023-08-09 15:23:46 +0200

    Code cleaned. Removed vega config files.

--- activate-chase-env.sh
+++
@@ -1,8 +0,0 @@
-spack load cmake git
-spack load gcc@11.2.0
-spack load /wz74dhn
-spack load intel-oneapi-compilers@2022.0.1
-spack load intel-oneapi-mkl@2022.2.0
-spack load cuda@11.6.0
-spack load openmpi@4.1.2
-spack load /3ejrwal
