522f2b3be5f9632583aa276cb62c33820adf984e
Nenad Mijic <nmijic@irb.hr>
2024-03-11 17:52:37 +0100

    Update FetchGtest.cmake

--- cmake/external/Gtest/FetchGtest.cmake
+++ cmake/external/Gtest/FetchGtest.cmake
@@ -4,7 +4,7 @@ FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG main 
-    FIND_PACKAGE_ARGS NAMES GTest )
+    )
 
 FetchContent_GetProperties(googletest)
 
