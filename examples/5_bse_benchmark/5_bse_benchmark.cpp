0ca2877aa0697a4428b1eb5e9e70ee43f9bac799
Xinzhe Wu <xin.wu@fz-juelich.de>
2025-05-21 21:06:29 +0200

    Improve example 5 by using popl to parser command line arguments

--- examples/5_bse_benchmark/5_bse_benchmark.cpp
+++ examples/5_bse_benchmark/5_bse_benchmark.cpp
@@ -10,6 +10,7 @@
 #include <random>
 #include <type_traits>
 #include <vector>
+#include "popl.hpp"
 
 #include "algorithm/performance.hpp"
 
@@ -23,7 +24,6 @@
 #include "Impl/chase_cpu/chase_cpu.hpp"
 #endif
 
-using T = std::complex<double>;
 using namespace chase;
 
 #if defined(HAS_CUDA) || defined(HAS_NCCL)
@@ -36,17 +36,61 @@ using ARCH = chase::platform::CPU;
 using BackendType = chase::grid::backend::NCCL;
 #endif
 
-int main(int argc, char** argv)
-{
+using namespace popl;
 
-    std::string file_path(argv[1]);	
-    size_t N = atoi(argv[2]), nev = atoi(argv[3]), nex = atoi(argv[4]);
+struct BSE_DriverProblemConfig
+{
+    std::size_t N; // Size of the Matrix
+    std::size_t nev; // Number of sought after eigenvalues
+    std::size_t nex; // Extra size of subspace
+    std::size_t deg; // initial degree
+
+    float tol_f; // desired tolerance
+    double tol_d; // desired tolerance
+    std::size_t maxIter; // maximum number of subspace iterations within ChASE.
+    std::size_t maxDeg; // maximum value of the degree of the Chebyshev filter
+    std::string opt; // enable optimisation of degree
+    std::string mode; // Approx or Random mode
+    std::size_t lanczosIter; // number of lanczos iterations
+    std::size_t numLanczos; // number of lanczos vectors
+    std::size_t blocksize; // blocksize
+    float lowerb_decay; // number of processes
+
+    std::string path_in; // path to the matrix input files
+    bool isdouble;       
+};
+
+template <typename T>
+int bse_solve(BSE_DriverProblemConfig& conf)
+{
+    std::size_t N = conf.N;
+    std::size_t nev = conf.nev;
+    std::size_t nex = conf.nex;
+    std::size_t deg = conf.deg;
+    std::size_t maxDeg = conf.maxDeg;
+    std::size_t maxIter = conf.maxIter;
+    chase::Base<T> tol; // desired tolerance
+    std::string opt = conf.opt;
+    std::string mode = conf.mode;
+    std::size_t mb = conf.blocksize;
+    float lowerb_decay = conf.lowerb_decay;
+    if (conf.isdouble)
+    {
+        tol = conf.tol_d;
+    }
+    else
+    {
+        tol = conf.tol_f;
+    }
+
+    std::size_t lanczosIter = conf.lanczosIter;
+    std::size_t numLanczos = conf.numLanczos;
+
+    std::string path_in = conf.path_in;
+    int world_rank = 0;
 
 #if defined(USE_MPI) || defined(HAS_NCCL)
-
-    MPI_Init(&argc, &argv);
-
-    int world_rank;
+    //setup MPI grid
     int world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -54,8 +98,8 @@ int main(int argc, char** argv)
     int dims_[2];
     dims_[0] = dims_[1] = 0;
 
-    MPI_Dims_create(world_size, 2, dims_); 
-    
+    MPI_Dims_create(world_size, 2, dims_);
+
     std::shared_ptr<chase::grid::MpiGrid2D<chase::grid::GridMajor::ColMajor>>
         mpi_grid = std::make_shared<
             chase::grid::MpiGrid2D<chase::grid::GridMajor::ColMajor>>(
@@ -63,14 +107,12 @@ int main(int argc, char** argv)
 
     int* dims = mpi_grid.get()->get_dims();
     int* coords = mpi_grid.get()->get_coords();
+#endif
 
     if (world_rank == 0)
     {
-
-#endif
 #if defined(USE_MPI) || defined(HAS_NCCL) //Parallel Implementation //A
-    std::cout << "======================== Dist ChASE ";
-
+        std::cout << "======================== Dist ChASE ";
 #if defined(USE_BLOCKCYCLIC) 
     std::cout << "BlockCyclic ";
 #else
@@ -78,8 +120,8 @@ int main(int argc, char** argv)
 #endif
 
 #else
-    std::cout << "======================== Seq  ChASE ";
-#endif
+        std::cout << "======================== Seq  ChASE ";
+#endif        
 
 #if defined(USE_QUASI_HERMITIAN) //C
     std::cout << "Quasi-Hermitian ";
@@ -94,17 +136,17 @@ int main(int argc, char** argv)
 #else
     std::cout <<  "on CPU ========================" << std::endl;
 #endif //B
-#if defined(USE_MPI) || defined(HAS_NCCL)
+  
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-#endif
+    std::chrono::high_resolution_clock::time_point start, end;
+    std::chrono::duration<double> elapsed;
+    
     auto Lambda = std::vector<chase::Base<T>>(nev + nex);
 
 #if defined(USE_MPI) || defined(HAS_NCCL) 
     
 #ifdef USE_BLOCKCYCLIC 
-    std::size_t mb = 64;
     auto Vec = chase::distMultiVector::DistMultiVectorBlockCyclic1D<
         T, chase::distMultiVector::CommunicatorType::column, ARCH>(N, nev + nex, mb, mpi_grid);
 #ifdef USE_QUASI_HERMITIAN 
@@ -133,8 +175,15 @@ int main(int argc, char** argv)
 #else
     auto single = chase::Impl::pChASECPU(nev, nex, &Hmat, &Vec, Lambda.data());
 #endif 
-    Hmat.readFromBinaryFile(file_path);
 
+    start = std::chrono::high_resolution_clock::now();
+    Hmat.readFromBinaryFile(path_in);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end - start;
+    if (world_rank == 0)
+    {
+        std::cout << "Time taken to read matrix: " << elapsed.count() << " seconds" << std::endl;
+    }
 #ifdef HAS_NCCL
     Hmat.H2D();
 #endif
@@ -148,7 +197,7 @@ int main(int argc, char** argv)
     using MatrixType = chase::matrix::Matrix<T,ARCH>;
 #endif //B
     
-    auto Hmat = new MatrixType(N,N);
+    auto Hmat = new MatrixType(N, N);
 
 #ifdef HAS_CUDA 
     auto single = chase::Impl::ChASEGPU<T,MatrixType>(N, nev, nex, Hmat, V.data(), N, Lambda.data());
@@ -157,8 +206,14 @@ int main(int argc, char** argv)
 #else 
     auto single = chase::Impl::ChASECPU<T,MatrixType>(N, nev, nex, Hmat, V.data(), N, Lambda.data());
 #endif
-	
-    Hmat->readFromBinaryFile(file_path);
+    start = std::chrono::high_resolution_clock::now();
+    Hmat->readFromBinaryFile(path_in);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end - start;
+    if (world_rank == 0)
+    {
+        std::cout << "Time taken to read matrix: " << elapsed.count() << " seconds" << std::endl;
+    }
 
 #ifdef HAS_CUDA
     Hmat->H2D();
@@ -166,40 +221,33 @@ int main(int argc, char** argv)
 
 #endif
 
-    std::cout << std::endl; 
-    
-    single.initVecs(true);
-
-    // Setup configure for ChASE
-    auto& config = single.GetConfig();
-    // Tolerance for Eigenpair convergence
-    config.SetTol(1e-10);
-    // Initial filtering degree
-    config.SetDeg(atoi(argv[5]));
-    // Optimi(S)e degree
-    if(argv[6][0] == 'Y'){
-        config.SetOpt(true);
-    }else{
-        config.SetOpt(false);
+    if (world_rank == 0)
+    {
+        std::cout << "Starting ChASE" << std::endl;
     }
 
-    config.SetMaxIter(25);
-    config.SetLanczosIter(26);
-
+    auto& config = single.GetConfig();
+    config.SetTol(tol);
+    config.SetDeg(deg);
+    config.SetOpt(opt == "S");
+    config.SetMaxIter(maxIter);
+    config.SetLanczosIter(lanczosIter);
+    config.SetNumLanczos(numLanczos);
+    config.SetMaxDeg(maxDeg);
+    config.SetApprox(mode == "A");
+    config.SetDecayingRate(lowerb_decay);
+    
     PerformanceDecoratorChase<T> performanceDecorator(&single);
 
     chase::Solve(&performanceDecorator);
 
-#if defined(USE_MPI) || defined(HAS_NCCL)
-    // Output
     if (world_rank == 0)
     {
-#endif
         performanceDecorator.GetPerfData().print();
         Base<T>* resid = single.GetResid();
-        std::cout << "Finished Problem #1"
+        std::cout << "Finished Problem #"
                   << "\n";
-        std::cout << "Printing first 10 eigenvalues and residuals\n";
+        std::cout << "Printing first 40 eigenvalues and residuals\n";
         std::cout
             << "| Index |       Eigenvalue      |         Residual      |\n"
             << "|-------|-----------------------|-----------------------|\n";
@@ -208,15 +256,125 @@ int main(int argc, char** argv)
         std::cout << std::setfill(' ');
         std::cout << std::scientific;
         std::cout << std::right;
-        for (auto i = 0; i < std::min(std::size_t(10), nev); ++i)
+        for (auto i = 0; i < std::min(std::size_t(40), nev); ++i)
             std::cout << "|  " << std::setw(4) << i + 1 << " | "
                       << std::setw(width) << Lambda[i] << "  | "
                       << std::setw(width) << resid[i] << "  |\n";
         std::cout << "\n\n\n";
+    }
+    return 0;
+    
+}
+
+int main(int argc, char** argv)
+{
 #if defined(USE_MPI) || defined(HAS_NCCL)
+
+    MPI_Init(&argc, &argv);
+#endif
+
+    BSE_DriverProblemConfig conf;
+
+    popl::OptionParser desc("ChASE options");
+    auto help_option = desc.add<Switch>("h", "help", "show this message");
+    desc.add<Value<std::size_t>, Attribute::required>("", "n", "Size of the Input Matrix", 0, &conf.N);
+    desc.add<Value<std::size_t>>("", "nev", "Wanted Number of Eigenpairs", 0, &conf.nev);
+    desc.add<Value<std::size_t>>("", "nex", "Extra Search Dimensions", 25, &conf.nex);
+    desc.add<Value<std::size_t>>("", "deg", "Initial filtering degree", 20, &conf.deg);
+    desc.add<Value<std::size_t>>("", "maxDeg", "Sets the maximum value of the degree of the Chebyshev filter", 36, &conf.maxDeg);
+    desc.add<Value<std::string>>("", "opt", "Optimi(S)e degree, or do (N)ot optimise", "S", &conf.opt);
+    desc.add<Value<std::string>>("", "mode", "Approximate (A) or Random (R)", "R", &conf.mode);
+    desc.add<Value<std::size_t>>("", "lanczosIter", "Sets the number of Lanczos iterations executed by ChASE.", 26, &conf.lanczosIter);
+    desc.add<Value<std::size_t>>("", "numLanczos", "Sets the number of stochastic vectors used for the spectral estimates in Lanczos", 4, &conf.numLanczos);
+    desc.add<Value<float>>("", "tol_f", "Sets the desired tolerance for float precision", 1e-5, &conf.tol_f);
+    desc.add<Value<double>>("", "tol_d", "Sets the desired tolerance for double precision", 1e-10, &conf.tol_d);
+    desc.add<Value<bool>>("", "double", "Is matrix double valued, false indicates the single type", true, &conf.isdouble);    
+    desc.add<Value<std::size_t>>("", "blocksize", "Sets the blocksize for the matrix", 64, &conf.blocksize);
+    desc.add<Value<float>>("", "lowerb_decay", "Sets the decaying rate for the lower bound", 1.0, &conf.lowerb_decay);
+    auto path_in_options = desc.add<Value<std::string>, Attribute::required>("", "path_in", "Path to the input matrix/matrices", "d", &conf.path_in);
+    
+    try
+    {
+        desc.parse(argc, argv);
+
+        if (help_option->count() == 1)
+        {
+            std::cout << desc << "\n";
+            return 0;
+        }
+    }
+    catch (const popl::invalid_option& e)
+    {
+        if (help_option->count() == 1)
+        {
+            std::cout << desc << "\n";
+            return 0;
+        }
+        std::cerr << "Invalid Option Exception: " << e.what() << "\n";
+        std::cerr << "error:  ";
+        if (e.error() == popl::invalid_option::Error::missing_argument)
+            std::cerr << "missing_argument\n";
+        else if (e.error() == popl::invalid_option::Error::invalid_argument)
+            std::cerr << "invalid_argument\n";
+        else if (e.error() == popl::invalid_option::Error::too_many_arguments)
+            std::cerr << "too_many_arguments\n";
+        else if (e.error() == popl::invalid_option::Error::missing_option)
+            std::cerr << "missing_option\n";
+
+        if (e.error() == popl::invalid_option::Error::missing_option)
+        {
+            std::string option_name(e.option()->name(popl::OptionName::short_name, true));
+            if (option_name.empty())
+                option_name = e.option()->name(popl::OptionName::long_name, true);
+            std::cerr << "option: " << option_name << "\n";
+        }
+        else
+        {
+            std::cerr << "option: " << e.option()->name(e.what_name()) << "\n";
+            std::cerr << "value:  " << e.value() << "\n";
+        }
+        return EXIT_FAILURE;
+    }
+    catch (const std::exception& e)
+    {
+        if (help_option->count() == 1)
+        {
+            std::cout << desc << "\n";
+            return 0;
+        }
+
+        std::cerr << "Exception: " << e.what() << "\n";
+        return EXIT_FAILURE;
     }
 
-    MPI_Finalize();
+    conf.opt = toupper(conf.opt.at(0));
+    conf.mode = toupper(conf.mode.at(0));
 
-#endif
+    if (conf.mode != "R" && conf.mode != "A")
+    {
+        std::cout << "Illegal value for mode: \"" << conf.mode << "\""
+                  << std::endl
+                  << "Legal values are R or A" << std::endl;
+        return -1;
+    }
+
+    if (conf.opt != "N" && conf.opt != "S")
+    {
+        std::cout << "Illegal value for opt: " << conf.opt << std::endl
+                  << "Legal values are N, S" << std::endl;
+        return -1;
+    }
+
+    if (conf.isdouble)
+    {
+        bse_solve<std::complex<double>>(conf);
+    }
+    else
+    {
+        bse_solve<std::complex<float>>(conf);
+    }
+
+#if defined(USE_MPI) || defined (HAS_NCCL)
+    MPI_Finalize();
+#endif    
 }
